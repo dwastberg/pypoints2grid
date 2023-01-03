c5559a9fdf28d9a6dfe37ea0c6d53437a1789933
Dag Wästberg <dwastberg@gmail.com>
2023-01-03 21:44:31 +0100

    add performance statistics

--- tests/performance.py
+++ tests/performance.py
@@ -8,12 +8,23 @@ np.random.seed(0)
 pts = np.random.rand(5000000, 3)
 pts[:,:2] *= 100
 
+# warm up
+dem = points2grid(pts, 0.1)
 
 times = []
-for i in range(3):
+for i in range(7):
     start = time.time()
     dem = points2grid(pts, 0.1)
     times.append(time.time() - start)
 
-print(f"points2grid took {mean(times)} seconds with a standard deviation of {stdev(times)}")
+print(f"points2grid just idw took {mean(times)} seconds with a standard deviation of {stdev(times)}")
+print(dem.shape)
+
+times = []
+for i in range(7):
+    start = time.time()
+    dem = points2grid(pts, 0.1, grid_data=['idw', 'min', 'max'])
+    times.append(time.time() - start)
+
+print(f"points2grid multi data took {mean(times)} seconds with a standard deviation of {stdev(times)}")
 print(dem.shape)
